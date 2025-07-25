fb86615e1c2c8e88da6d54d50b76df3cd09d78ab
Salvador Bernabel <salvadorbernabel@gmail.com>
2025-07-25 22:41:43 +0000

    adding app, server, and config

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, { Express, Router } from 'express'
+import { errorHandler } from './middlewares/error-handler.middleware.js'
+
+interface AppDeps {
+  apiRouter: Router,
+  redirectRouter: Router
+}
+
+export function createApp({ apiRouter, redirectRouter }: AppDeps): Express {
+  const app: Express = express()
+
+  // middlewares
+  app.use(express.json())
+
+  // Routes
+  app.use('/api', apiRouter)
+  app.use('/', redirectRouter)
+
+  // Global error handler
+  app.use(errorHandler)
+
+  return app
+}
---
+++ src/config/config.ts
@@ -0,0 +1,17 @@
+import { cleanEnv, port, str } from 'envalid'
+
+const env = cleanEnv(process.env, {
+  PORT: port({ default: 3000 }),
+  REDIS_HOST: str(),
+  REDIS_PORT: port({ default: 6739 }),
+  REDIS_USERNAME: str(),
+  REDIS_PASSWORD: str()
+})
+
+export const config = {
+  port: env.PORT,
+  redisHost: env.REDIS_HOST,
+  redisPort: env.REDIS_PORT,
+  redisUsername: env.REDIS_USERNAME,
+  redisPassword: env.REDIS_PASSWORD
+}
