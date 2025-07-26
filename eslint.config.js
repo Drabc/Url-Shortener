a7edddb5fb5dfd1a8e5422ccbdacd485572db064
Salvador Bernabel <salvadorbernabel@gmail.com>
2025-07-26 16:09:35 +0000

    eslint setup

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+import typescriptParser from '@typescript-eslint/parser'
+import typescriptPlugin from '@typescript-eslint/eslint-plugin'
+import importPlugin from 'eslint-plugin-import'
+import prettierPlugin from 'eslint-plugin-prettier'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores([
+    'eslint.config.js', 'node_modules/*'
+  ]),
+  {
+    files: ['src/**/*.ts'],
+
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: process.cwd(),
+        sourceType: 'module',
+      },
+    },
+
+    plugins: {
+      '@typescript-eslint': typescriptPlugin,
+      import: importPlugin,
+      prettier: prettierPlugin,
+    },
+
+    rules: {
+      // Bring in each plugin’s recommended rules
+      ...typescriptPlugin.configs.recommended.rules,
+      ...importPlugin.configs.recommended.rules,
+      ...prettierPlugin.configs.recommended.rules,
+
+      // overrides
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'import/order': ['error', {
+        groups: ['builtin','external','internal','parent','sibling','index'],
+        'newlines-between': 'always'
+      }],
+      'quotes': ['error', 'single', { avoidEscape: true }],
+      'semi': ['error', 'never'],
+    },
+
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json',
+        },
+      },
+    },
+  },
+])
