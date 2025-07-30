676b931f31e7a96cb9c82f1e97501f9d83945b10
Salvador Bernabel <salvadorbernabel@gmail.com>
2025-07-30 17:31:37 +0000

    reorganizing structure

--- src/repositories/url-repository.interface.ts
+++
@@ -1,6 +0,0 @@
-import { ShortUrl } from '../domain/entities/short-url.js'
-
-export interface IUrlRepository {
-  save(entity: ShortUrl): Promise<void>
-  findById(id: string): Promise<ShortUrl | null>
-}
