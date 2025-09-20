591c3ebb7740257e361acf4005d2a50a938b9b65
Salvador Bernabel <salvadorbernabel@gmail.com>
2025-09-20 19:58:05 +0000

    Adding pg client errors

---
+++ src/infrastructure/errors/pg-client.error.ts
@@ -0,0 +1,25 @@
+import { BaseError, ErrorKinds } from '@shared/errors.js'
+
+/**
+ * Error thrown when a PostgreSQL transaction cannot be started.
+ * @augments {BaseError}
+ */
+export class PgTransactionBeginError extends BaseError {
+  constructor(cause?: unknown) {
+    const stack = cause instanceof Error ? cause.stack : undefined
+    super(ErrorKinds.system, 'PG_TX_BEGIN_FAILURE', 'Unable to start transaction', stack)
+    this.name = 'PgTransactionBeginError'
+  }
+}
+
+/**
+ * Error thrown when a PostgreSQL transaction cannot be committed.
+ * @augments {BaseError}
+ */
+export class PgTransactionCommitError extends BaseError {
+  constructor(cause?: unknown) {
+    const stack = cause instanceof Error ? cause.stack : undefined
+    super(ErrorKinds.system, 'PG_TX_COMMIT_FAILURE', 'Failed to commit transaction', stack)
+    this.name = 'PgTransactionCommitError'
+  }
+}
