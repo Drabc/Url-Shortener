3ebe583ad74476b096739e8472272a44f5b7557d
Salvador Bernabel <salvadorbernabel@gmail.com>
2025-10-15 23:25:37 +0000

    Error clean up

--- src/application/errors/base-application.error.ts
+++ src/application/errors/base-application.error.ts
@@ -1,5 +1,3 @@
 import { BaseErrorV2 } from '@shared/errors.js'
 
 export type BaseApplicationError<T extends string> = BaseErrorV2<'application', T>
-
-export type ApplicationError = BaseApplicationError<'InvalidSession'>
